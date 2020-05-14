c140710127e8a4dc665ce634777fcd057e0d12db
lizhiqiang <lizhiqiang-it@bestpay.com.cn>
2020-05-14 14:51:55 +0800

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # yekai
 redis lua 流量控制 秒杀 库存
+
+
+支持表达式语法，并且，或，大于、小于、等于。。。。
+
+            &&           
+         /      \         
+      ==            ||     
+    /   \        /      \    
+  ICBC    BANK  ==        == 
+              /    \     / \ 
+            TRADE COED 201 TYPE
+
+((TYPE==201)||(COED==TRADE))&&(BANK==ICBC)--> 5tps
--- yekai-service/src/test/java/com/yekai/limiter/service/symbol/TreeNodeSwapTest.java
+++ yekai-service/src/test/java/com/yekai/limiter/service/symbol/TreeNodeSwapTest.java
@@ -19,28 +19,28 @@ public class TreeNodeSwapTest {
     @Test
     public void testSwap() {
         TreeNode a = ExpressionTest.express("a");
-//        TreePrinter.show(a);
+        TreePrinter.show(a);
         Expression.printMathExpression(a);
         TreeNodeSwap.swap1(a);
         System.out.print("-->");
         Expression.printMathExpression(a);
         System.out.println();
 
-        TreeNode b = ExpressionTest.express("b");
-//        TreePrinter.show(b);
-        Expression.printMathExpression(b);
-        TreeNodeSwap.swap1(b);
-        System.out.print("-->");
-        Expression.printMathExpression(b);
-        System.out.println();
-
-        TreeNode c = ExpressionTest.express("c");
-//        TreePrinter.show(c);
-        Expression.printMathExpression(c);
-        TreeNodeSwap.swap1(c);
-        System.out.print("-->");
-        Expression.printMathExpression(c);
-        System.out.println();
+//        TreeNode b = ExpressionTest.express("b");
+////        TreePrinter.show(b);
+//        Expression.printMathExpression(b);
+//        TreeNodeSwap.swap1(b);
+//        System.out.print("-->");
+//        Expression.printMathExpression(b);
+//        System.out.println();
+//
+//        TreeNode c = ExpressionTest.express("c");
+////        TreePrinter.show(c);
+//        Expression.printMathExpression(c);
+//        TreeNodeSwap.swap1(c);
+//        System.out.print("-->");
+//        Expression.printMathExpression(c);
+//        System.out.println();
     }
 
 
